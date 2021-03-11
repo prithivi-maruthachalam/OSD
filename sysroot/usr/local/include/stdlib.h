ef1f32ab1d36bd2a29d536866e57bbfa89fdefac
prythagoras <prithivimaruthachalam@gmail.com>
2021-03-11 18:14:30 +0530

    Removed accidental files

--- sysroot/usr/local/include/stdlib.h
+++
@@ -1,9 +0,0 @@
-#ifndef STDLIB_H
-#define STDLIB_H 1
-
-#include <sys/cdefs.h>
-
-// Kernel panic - abnormally terminate process like SIGABRT
-__attribute__((__noreturn__)) void abort();
-
-#endif
--- sysroot/usr/local/include/sys/cdefs.h
+++
@@ -1,7 +0,0 @@
-#ifndef CDEFS_H
-#define CDEFS_H 1
-
-// libc vs libk distinction
-#define _is_libc 1
-
-#endif
