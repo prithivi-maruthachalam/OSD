28f1b27e2524029fdaa94753c91ce8e935d1ed21
prythagoras <prithivimaruthachalam@gmail.com>
2021-03-26 13:49:38 +0530

    Added TSS descriptor to GDT

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -1,12 +1,13 @@
 #include <kernel/gdt.h>
 
 struct gdtDescriptor GDTR;
-struct gdtEntry GDT_entries[5];
+struct gdtEntry GDT_entries[6];
+volatile struct TSS_segment TSS;
 
 extern void load_gdt(struct gdtDescriptor *);
 
 // to create each entry()
-void gdt_create_entry(struct gdtEntry *entry, uint32_t base, uint32_t limit, uint8_t access)
+void gdt_create_entry(struct gdtEntry *entry, uint32_t base, uint32_t limit, uint8_t type, uint8_t flags)
 {
     // setting base
     entry->baseLow = (uint16_t)base & 0x0000FFFF;
@@ -18,21 +19,23 @@ void gdt_create_entry(struct gdtEntry *entry, uint32_t base, uint32_t limit, uin
     entry->limitHigh_flags = (uint8_t)(limit >> 16) & 0x0000000F;
 
     // flags
-    entry->limitHigh_flags |= 0xC0;
+    entry->limitHigh_flags |= flags;
 
-    // access flags
-    entry->access = access;
+    // type flags
+    entry->type = type;
 }
 
 void gdt_init()
 {
-    // set up entries
-    gdt_create_entry(&GDT_entries[0], 0, 0, 0);
-    gdt_create_entry(&GDT_entries[1], 0, 0xFFFFFFFF, 0x9A); // kernel code
-    gdt_create_entry(&GDT_entries[2], 0, 0xFFFFFFFF, 0x92); // kernel data
-    gdt_create_entry(&GDT_entries[3], 0, 0xFFFFFFFF, 0xFA); // user code
-    gdt_create_entry(&GDT_entries[4], 0, 0xFFFFFFFF, 0xF2); // user data
-    // TODO: Add TSS descriptor
+    // set up gdt entries
+    // entry pointer | base | limit | type | flags
+    gdt_create_entry(&GDT_entries[0], 0, 0, 0, 0);
+    gdt_create_entry(&GDT_entries[1], 0, 0xFFFFFFFF, 0x9A, 0xC0); // kernel code
+    gdt_create_entry(&GDT_entries[2], 0, 0xFFFFFFFF, 0x92, 0xC0); // kernel data
+    gdt_create_entry(&GDT_entries[3], 0, 0xFFFFFFFF, 0xFA, 0xC0); // user code
+    gdt_create_entry(&GDT_entries[4], 0, 0xFFFFFFFF, 0xF2, 0xC0); // user data
+    gdt_create_entry(&GDT_entries[5], (uint32_t)&TSS, (sizeof(TSS) - 1), 0x89, 0x10);
+    TSS.SS0 = 0x10; // kernel data
 
     GDTR.limit = sizeof(GDT_entries) - 1;
     GDTR.baseAddress = (uint32_t)&GDT_entries;
--- kernel/include/kernel/gdt.h
+++ kernel/include/kernel/gdt.h
@@ -12,7 +12,7 @@ struct gdtEntry
     uint16_t limitLow;
     uint16_t baseLow;
     uint8_t baseMid;
-    uint8_t access;
+    uint8_t type;
     uint8_t limitHigh_flags;
     uint8_t baseHigh;
 } __attribute__((packed));
@@ -22,6 +22,64 @@ struct gdtDescriptor
 {
     uint16_t limit;
     uint32_t baseAddress;
+} __attribute__((packed));
+
+struct TSS_segment
+{
+    // Previous TSS selector (obsolete, because we're only making one TSS)
+    uint16_t taskLink;
+    uint16_t taskLink_high_reserved; // Reserved
+
+    uint32_t ESP0;
+    uint16_t SS0;
+    uint16_t SS0_high_reserved; // Reserved
+
+    uint32_t ESP1;
+    uint16_t SS1;
+    uint16_t SS1_high_reserved; // Reserved
+
+    uint32_t ESP2;
+    uint16_t SS2;
+    uint16_t SS2_high_reserved; // Reserved
+
+    uint32_t CR3; // Base of the paging structure
+    uint32_t EIP;
+    uint32_t EFLAGS;
+    uint32_t EAX;
+    uint32_t ECX;
+    uint32_t EDX;
+    uint32_t EBX;
+    uint32_t ESP;
+    uint32_t EBP;
+    uint16_t ESI;
+    uint32_t EDI;
+
+    // segment selector registers
+    uint16_t ES;
+    uint16_t ES_high_reserved;
+
+    uint16_t CS;
+    uint16_t CS_high_reserved;
+
+    uint16_t SS;
+    uint16_t SS_high_reserved;
+
+    uint16_t DS;
+    uint16_t DS_high_reserved;
+
+    uint16_t FS;
+    uint16_t FS_high_reserved;
+
+    uint16_t GS;
+    uint16_t GS_high_reserved;
+
+    // LDT segment selector
+    uint16_t LDT;
+    uint16_t LDT_high_reserved;
+
+    // IO map
+    uint16_t IOMap_low_reserved;
+    uint16_t IO_Map;
 } __attribute__((packed));
 
 #endif
